8f96ced011d02faf4911c8553793ad85740d53e1
Gandharv Kulkarni <gandharvakulkarni2003@gmail.com>
2024-04-19 21:54:35 +0530

    Payment Config changed

--- backend/config/razorpay_config.py
+++ backend/config/razorpay_config.py
@@ -1,4 +1,4 @@
 
 import razorpay
 
-razorpay_client = razorpay.Client(auth=("rzp_test_mjQDXiVvOSGgd4", "XX5uPa4fOejDB63vFqrUG2HR"))
+razorpay_client = razorpay.Client(auth=("key_id", "key_secret"))
